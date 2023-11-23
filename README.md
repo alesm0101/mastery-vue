436b8057aff908f1891d5e8ea1be0e04b8b04164
alesm0101 <alesm0101@gmail.com>
2023-11-23 13:00:49 +0100

    mock data events

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 ## rw-vue3-composition
 `npm run dev --workspace=rw-vue3-composition`
+
 `npm run test:unit --workspace=rw-vue3-composition`
--- rw-vue3-composition/src/App.vue
+++ rw-vue3-composition/src/App.vue
@@ -9,7 +9,7 @@ import { RouterLink, RouterView } from 'vue-router'
       <div class="wrapper">
 
         <nav>
-          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/">EventListView</RouterLink>
           <RouterLink to="/about">About</RouterLink>
         </nav>
       </div>
